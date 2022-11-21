9d24e1a8b583a6abf21c4187d4cc2cf909d65e02
Malav Shah <mdshah9574@gmail.com>
2022-11-22 00:29:41 +0530

    clean: state

--- src/features/Dashboard/components/Container/Container.component.tsx
+++ src/features/Dashboard/components/Container/Container.component.tsx
@@ -6,80 +6,43 @@ import DataGrid from "@atomics/DataGrid";
 import PaginationGrid from "@atomics/PaginationGrid";
 import Button from "@atomics/Button";
 
-import { searchInAdminItem } from "@util/helper";
 import COPY_TEXT from "@util/copyText";
+
+import { getAdminData, AdminItem } from "@APIs/Users.api";
+import useAsync from "@hooks/useAsync.hooks";
+
+import useUsers from "./useUsers.hooks";
 
 import { AdminColumnsData } from "./Dashboard.static";
 import container from "./Container.module.scss";
-import { getAdminData, AdminItem } from "@APIs/Users.api";
-import useAsync from "@hooks/useAsync.hooks";
-import useUsers from "./useUsers.hooks";
 
 type ContainerProps = {
   searchString: string;
 };
 
 const Container = ({ searchString }: ContainerProps) => {
-  const { data, error, isLoading, isIdle, isError, run } = useAsync<
+  const { data, error, isLoading, isError, run } = useAsync<
     Array<AdminItem>
   >({ data: [], error: null, status: "idle" });
-  const { users, onSearch, setInitialData } = useUsers();
 
-  const [pageSize, setPageSize] = useState(10);
+  const { users, totalCount, onSearch, setInitialData, onChangePageNumber, pageNumber, pageSize, onChangePageSize} = useUsers();
+
   const [selection, setSelection] = useState<Array<AdminItem["id"]>>([]);
 
-  const [selectedPage, setSelectedPage] = useState(0);
-  const [actualData, setActualData] = useState<AdminItem[]>([]);
-  const [totalCount, setTotalCount] = useState(data ? data.length : 0);
 
   useEffect(() => {
     run(getAdminData());
   }, []);
 
-  useEffect(() => {
-    if (data) {
-      setInitialData(data);
-    }
-  }, [data]);
+  useEffect(()=>{
+      if(data){
+        setInitialData(data, pageSize)
+      }
+  },[data])
 
   useEffect(() => {
-    const tempActualData = (
-      searchString && data
-        ? data.filter((item) => searchInAdminItem(searchString, item))
-        : data || []
-    ).slice(0, 10);
-
-    setActualData(tempActualData);
-  }, [data, searchString]);
-
-  useEffect(() => {
-    if (selectedPage !== 0) {
-      setSelectedPage(0);
-    }
+      onSearch(searchString)
   }, [searchString]);
-
-  useEffect(() => {
-    if (data) {
-      const startIndex = selectedPage * pageSize;
-
-      const filteredData = searchString
-        ? data.filter((item) => searchInAdminItem(searchString, item))
-        : data;
-
-      setTotalCount(filteredData.length);
-
-      const paginatedData = filteredData.slice(
-        startIndex,
-        startIndex + pageSize
-      );
-
-      setActualData(paginatedData);
-    }
-  }, [pageSize, selectedPage, data, searchString]);
-
-  const onChangePageSize = (pgSize: number) => setPageSize(pgSize);
-
-  const onChangePage = (pg: number) => setSelectedPage(pg);
 
   const onSelectionModeChange = (selectionParam: GridSelectionModel) => {
     setSelection(selectionParam as Array<string>);
@@ -95,7 +58,7 @@ const Container = ({ searchString }: ContainerProps) => {
   if (isError) {
     return <div>{error?.message || COPY_TEXT.ERRORS.callingAPI}</div>;
   }
-
+  
   return (
     <div className={container.container}>
       {selection.length ? (
@@ -111,16 +74,17 @@ const Container = ({ searchString }: ContainerProps) => {
         paginationMode="server"
         rowCount={totalCount}
         rowsPerPageOptions={[10]}
-        page={selectedPage}
+        page={pageNumber}
         pageSize={pageSize}
         editMode={"cell"}
         onPageSizeChange={onChangePageSize}
-        onPageChange={onChangePage}
+        onPageChange={onChangePageNumber}
         pagination={true}
-        rows={actualData}
+        rows={users}
         onCellEditStop={onCellEditStop}
         disableSelectionOnClick
         onSelectionModelChange={onSelectionModeChange}
+        loading={isLoading}
         components={{
           Pagination: PaginationGrid,
         }}
--- src/features/Dashboard/components/Container/useUsers.hooks.ts
+++ src/features/Dashboard/components/Container/useUsers.hooks.ts
@@ -5,6 +5,10 @@ import { searchInAdminItem } from "@util/helper";
 type AdminReducerType = {
   items: AdminItem[];
   initialItems: AdminItem[];
+  pageSize: number;
+  currentPage: number;
+  searchString: string;
+  totalCount: number;
 };
 
 type Action =
@@ -18,8 +22,19 @@ type Action =
       type: "INITIAL_LOADED";
       payload: {
         items: AdminItem[];
+        pageSize: number
       };
-    };
+  } | {
+      type: 'CHANGE_PAGE',
+      payload:{
+        newPage: number
+      }
+  } | {
+      type: 'CHANGE_PAGE_SIZE',
+      payload:{
+        newPageSize: number
+      }
+  };
 
 function UserReducer(
   prevState: AdminReducerType,
@@ -28,25 +43,61 @@ function UserReducer(
   switch (action.type) {
     case "SEARCH": {
       const { searchString } = action.payload;
+      const { pageSize, initialItems } = prevState;
       if (searchString) {
-        const filteredItems = prevState.initialItems.filter((item) =>
+        const filteredItems = initialItems.filter((item) =>
           searchInAdminItem(searchString, item)
         );
         return {
           ...prevState,
-          items: filteredItems,
+          items: filteredItems.slice(0, pageSize),
+          searchString: action.payload.searchString,
+          totalCount: filteredItems.length,
+          currentPage: 0
         };
       }
       return {
-        initialItems: prevState.initialItems,
-        items: prevState.initialItems,
+        ...prevState,
+        initialItems: initialItems,
+        items: prevState.initialItems.slice(0, prevState.pageSize),
+        searchString: searchString,
+        totalCount: initialItems.length,
+        currentPage: 0
       };
     }
     case "INITIAL_LOADED": {
+      const {pageSize, items} = action.payload
       return {
-        initialItems: action.payload.items,
-        items: action.payload.items,
+        ...prevState,
+        initialItems: items,
+        items: items.slice(0, pageSize),
+        pageSize: pageSize,
+        totalCount: items.length
       };
+    }
+    case 'CHANGE_PAGE':{
+      const {newPage} = action.payload
+      const {searchString, initialItems, pageSize} = prevState
+      let actualItems = initialItems;
+      const startIndex = newPage  * pageSize;
+      if(prevState.searchString){
+         actualItems  = initialItems.filter((item) =>
+          searchInAdminItem(searchString, item)
+        );
+      }
+      actualItems = actualItems.slice(startIndex, startIndex + pageSize);
+      return {
+        ...prevState,
+        currentPage: newPage,
+        items: actualItems
+      }
+    }
+    case 'CHANGE_PAGE_SIZE':{
+      const {newPageSize} = action.payload
+      return {
+        ...prevState,
+        pageSize: newPageSize
+      }
     }
     default: {
       return prevState;
@@ -55,10 +106,16 @@ function UserReducer(
 }
 
 function useUsers() {
-  const [users, dispatch] = useReducer(UserReducer, {
+  const [userData, dispatch] = useReducer(UserReducer, {
     items: [],
     initialItems: [],
+    pageSize: 10,
+    currentPage: 0,
+    searchString: '',
+    totalCount: 0
   });
+
+  const {items, totalCount, currentPage, pageSize} = userData
 
   const onSearch = (searchString: string) => {
     dispatch({
@@ -69,19 +126,44 @@ function useUsers() {
     });
   };
 
-  const setInitialData = (data: AdminItem[]) => {
+  const setInitialData = (data: AdminItem[], pageSize: number) => {
     dispatch({
       type: "INITIAL_LOADED",
       payload: {
         items: data,
+        pageSize: pageSize
       },
     });
   };
 
+  const onChangePageNumber = (page: number)=>{
+    dispatch({
+      type: 'CHANGE_PAGE',
+      payload:{
+        newPage: page
+      }
+    })
+  }
+
+  const onChangePageSize = (pageSize: number)=>{
+    dispatch({
+      type: 'CHANGE_PAGE_SIZE',
+      payload:{
+        newPageSize: pageSize
+      }
+    })
+  }
+
   return {
-    users,
+    users: items,
+    pageNumber: currentPage,
+    totalCount: totalCount,
+    pageSize: pageSize,
+
     onSearch,
     setInitialData,
+    onChangePageNumber,
+    onChangePageSize
   };
 }
 
